2ecd818620a62e15befc3e1705ee7f35ffa59889
Samayra312007 <samayra312007@gmail.com>
2025-07-13 16:05:31 +0530

    add queue reversal problem

---
+++ geeks_for_geeks/queue_reversal.cpp
@@ -0,0 +1,21 @@
+// function Template for C++
+
+class Solution
+{
+public:
+    queue<int> reverseQueue(queue<int> &q)
+    {
+        stack<int> st;
+        while (!(q.empty()))
+        {
+            st.push(q.front());
+            q.pop();
+        }
+        while (!(st.empty()))
+        {
+            q.push(st.top());
+            st.pop();
+        }
+        return q;
+    }
+};
