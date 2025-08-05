e2d55106f2cc20d8348fcaaf3ba908a3e84afae4
Samayra312007 <samayra312007@gmail.com>
2025-08-05 21:45:07 +0530

    add two odd occuring problem

---
+++ geeks_for_geeks/two_odd_occuring.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    vector<int> twoOddNum(vector<int> &arr)
+    {
+        int n = arr.size();
+        long long xorr = 0;
+        for (int i = 0; i < n; i++)
+        {
+            xorr ^= arr[i];
+        }
+        int rightmost = xorr & -xorr;
+        int b1 = 0;
+        int b2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] & rightmost)
+            {
+                b1 = b1 ^ arr[i];
+            }
+            else
+            {
+                b2 = b2 ^ arr[i];
+            }
+        }
+        if (b1 > b2)
+            return {b1, b2};
+        else
+            return {b2, b1};
+    }
+};
