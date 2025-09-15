fc1460b4b8bbaa76719352a75bc905fc7a14d17f
Samayra312007 <samayra312007@gmail.com>
2025-09-15 23:36:02 +0530

    add left view of binary tree

---
+++ geeks_for_geeks/left_view_of_binary_tree.cpp
@@ -0,0 +1,47 @@
+/*
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+ */
+
+class Solution
+{
+public:
+    vector<int> leftView(Node *root)
+    {
+        vector<int> ans;
+        if (root == nullptr)
+            return ans;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            ans.push_back(q.front()->data);
+            int n = q.size();
+            while (n--)
+            {
+                Node *temp = q.front();
+                q.pop();
+                if (temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if (temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+        return ans;
+    }
+};
