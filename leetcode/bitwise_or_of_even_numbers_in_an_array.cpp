c839703f14914031b7288c12d68d6afc6bb0fddb
Samayra312007 <samayra312007@gmail.com>
2025-09-21 18:39:43 +0530

    add bitwise or of even numbers in an array problem

---
+++ leetcode/bitwise_or_of_even_numbers_in_an_array.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int evenNumberBitwiseORs(vector<int> &nums)
+    {
+        int n = nums.size();
+        int nor = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                nor = nor | nums[i];
+            }
+        }
+        return nor;
+    }
+};
