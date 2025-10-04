e468412192862e11b276e726e93569a9306bafcb
Samayra312007 <samayra312007@gmail.com>
2025-10-04 10:48:23 +0530

    add preorder and  bst problem

---
+++ geeks_for_geeks/preorder_and_bst.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+class Solution
+{
+public:
+    void BST(int arr[], int &index, int lower, int upper, int N)
+    {
+        if (index == N || arr[index] < lower || arr[index] > upper)
+        {
+            return;
+        }
+        int value = arr[index++];
+        BST(arr, index, lower, value, N);
+        BST(arr, index, value, upper, N);
+    }
+    int canRepresentBST(int arr[], int N)
+    {
+        int index = 0;
+        BST(arr, index, INT_MIN, INT_MAX, N);
+        return index == N;
+    }
+};
