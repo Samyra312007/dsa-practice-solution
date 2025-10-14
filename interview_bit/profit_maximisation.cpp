c1952fdf3217728f027734eec0b64fa0ecef3369
Samyra312007 <samayra312007@gmail.com>
2025-10-14 15:51:19 +0530

    add profit maximisation problem

---
+++ interview_bit/profit_maximisation.cpp
@@ -0,0 +1,20 @@
+int Solution::solve(vector<int> &A, int B)
+{
+    int sum = 0;
+    priority_queue<int> p;
+    for (int i = 0; i < A.size(); i++)
+    {
+        p.push(A[i]);
+    }
+    while (B && !p.empty())
+    {
+        sum += p.top();
+        if (p.top() - 1)
+        {
+            p.push(p.top() - 1);
+        }
+        p.pop();
+        B--;
+    }
+    return sum;
+}
