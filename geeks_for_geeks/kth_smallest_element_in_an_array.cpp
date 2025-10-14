cc9a2a30bdc7036e0d003686baad7d9b798f279a
Samyra312007 <samayra312007@gmail.com>
2025-10-14 15:28:04 +0530

    add kth smallest element in array problem

---
+++ geeks_for_geeks/kth_smallest_element_in_an_array.cpp
@@ -0,0 +1,21 @@
+// User function template for C++
+
+class Solution
+{
+public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k)
+    {
+        priority_queue<int, vector<int>, greater<int>> p;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            p.push(arr[i]);
+        }
+        while (--k)
+        {
+            p.pop();
+        }
+        return p.top();
+    }
+};
