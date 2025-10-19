7f01482bcdef535e70676518f18daead958d4a25
Samyra312007 <samayra312007@gmail.com>
2025-10-19 21:36:00 +0530

    add kth smallest  element in a sorted matrix problem

---
+++ leetcode/kth_smallest_element_in_a_sorted_matrix.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int kthSmallest(vector<vector<int>> &matrix, int k)
+    {
+        int n = matrix.size();
+        priority_queue<int> p;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                p.push(matrix[i][j]);
+                if (p.size() > k)
+                    p.pop();
+            }
+        }
+        return p.top();
+    }
+};
