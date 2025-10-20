16485f671c559791462af302c972f30bc3cef472
Samyra312007 <samayra312007@gmail.com>
2025-10-20 10:46:15 +0530

    add merge k sorted lists problem

---
+++ leetcode/merge_k_sorted_lists.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    class Compare
+    {
+    public:
+        bool operator()(ListNode *a, ListNode *b)
+        {
+            return a->val > b->val;
+        }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        priority_queue<ListNode *, vector<ListNode *>, Compare> p;
+        for (auto node : lists)
+        {
+            if (node != nullptr)
+            {
+                p.push(node);
+            }
+        }
+        ListNode *root = new ListNode(0);
+        ListNode *tail = root;
+        ListNode *temp;
+        while (!p.empty())
+        {
+            temp = p.top();
+            p.pop();
+            tail->next = temp;
+            tail = tail->next;
+            if (temp->next)
+            {
+                p.push(temp->next);
+            }
+        }
+        return root->next;
+    }
+};
