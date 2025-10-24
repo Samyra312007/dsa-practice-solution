63caaafdc2880c6d49ea51e24dd52bd8eeeb8411
Samyra312007 <samayra312007@gmail.com>
2025-10-24 13:42:52 +0530

    add number complement problem

---
+++ leetcode/complement_of_base10_integer.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int bitwiseComplement(int n)
+    {
+        if (n == 0)
+            return 1;
+        string bin = "";
+        while (n > 0)
+        {
+            if (n % 2 == 0)
+                bin += "0";
+            else
+                bin += "1";
+            n = n / 2;
+        }
+        bin += "1";
+        string res = "";
+        int ans = 0;
+        for (int i = bin.length() - 1; i >= 0; i--)
+        {
+            res += (bin[i] == '1') ? '0' : '1';
+        }
+        reverse(res.begin(), res.end());
+        for (int i = res.length() - 1; i >= 0; i--)
+        {
+            ans = ans + (res[i] - '0') * (1 << i);
+        }
+        return ans;
+    }
+};
