0e1a6357f525f7ef3a193fa0d2fbf3668b8e5b6c
Samyra312007 <samayra312007@gmail.com>
2025-10-29 22:30:53 +0530

    add bfs of a graph

---
+++ geeks_for_geeks/bfs_of_a_graph.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    vector<int> bfs(vector<vector<int>> &adj)
+    {
+        vector<int> bfs;
+        queue<int> q;
+        vector<int> vis(adj.size(), 0);
+        vis[0] = 1;
+        q.push(0);
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            for (auto it : adj[node])
+            {
+                if (!vis[it])
+                {
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
