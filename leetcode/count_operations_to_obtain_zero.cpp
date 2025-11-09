52015ab702ea70fd1c1ffe6b4c521d48c11a47b7
Samyra312007 <samayra312007@gmail.com>
2025-11-09 21:40:53 +0530

    add count operations to obtain zero

---
+++ leetcode/count_operations_to_obtain_zero.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int countOperations(int num1, int num2)
+    {
+        int operations = 0;
+        while (num1 != 0 && num2 != 0)
+        {
+            if (num1 == num2)
+            {
+                num2 = num2 - num1;
+                operations++;
+            }
+            else if (num1 > num2)
+            {
+                num1 = num1 - num2;
+                operations++;
+            }
+            else if (num2 > num1)
+            {
+                num2 = num2 - num1;
+                operations++;
+            }
+        }
+        return operations;
+    }
+};
