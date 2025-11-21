de595914eaeed0fe385345dd81b51d0828031d96
Samyra312007 <samayra312007@gmail.com>
2025-11-21 23:24:58 +0530

    add max consecutive ones problem

---
+++ leetcode/max_consecutive_ones.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int count = 0;
+        int tempcount = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 1)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+            tempcount = max(count, tempcount);
+        }
+        return tempcount;
+    }
+};
