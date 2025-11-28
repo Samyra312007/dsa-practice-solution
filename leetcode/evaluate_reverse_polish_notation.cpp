3e30e043b54493e11efd73a09214be5ee3ff1fc6
Samyra312007 <samayra312007@gmail.com>
2025-11-28 22:46:45 +0530

    add reverse polish notation problem

---
+++ leetcode/evaluate_reverse_polish_notation.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<int> st;
+        for (int i = 0; i < tokens.size(); i++)
+        {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
+            {
+                st.push(stoi(tokens[i]));
+            }
+            else
+            {
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                if (tokens[i] == "+")
+                {
+                    int num = op2 + op1;
+                    st.push(num);
+                }
+                else if (tokens[i] == "-")
+                {
+                    int num = op2 - op1;
+                    st.push(num);
+                }
+                else if (tokens[i] == "*")
+                {
+                    int num = op2 * op1;
+                    st.push(num);
+                }
+                else if (tokens[i] == "/")
+                {
+                    int num = op2 / op1;
+                    st.push(num);
+                }
+            }
+        }
+        return st.top();
+    }
+};
