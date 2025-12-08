ec976ede8c320eb579a73dde63693f248ba90a1d
Samyra312007 <samayra312007@gmail.com>
2025-12-08 20:00:38 +0530

    add minimum coins problem

---
+++ leetcode/minimum_coins.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<vector<int>> t(n + 1, vector<int>(amount + 1, 0));
+        for (int i = 0; i < amount + 1; i++)
+            t[0][i] = INT_MAX - 1;
+        for (int i = 1; i < amount + 1; i++)
+        {
+            if (i % coins[0] == 0)
+                t[1][i] = i / coins[0];
+            else
+                t[1][i] = INT_MAX - 1;
+        }
+        for (int i = 2; i < n + 1; i++)
+        {
+            for (int j = 1; j < amount + 1; j++)
+            {
+                if (coins[i - 1] <= j)
+                {
+                    t[i][j] = min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
+                }
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+        return (t[n][amount] == INT_MAX - 1 ? -1 : t[n][amount]);
+    }
+};
