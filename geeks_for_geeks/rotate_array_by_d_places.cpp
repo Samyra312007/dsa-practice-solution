06a87d826fb865bcf9a940353c54459502615fdd
Samyra312007 <samayra312007@gmail.com>
2026-06-25 19:42:45 +0530

    Add rotate array by d places problem solution

---
+++ geeks_for_geeks/rotate_array_by_d_places.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void reverse(vector<int>& arr, int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+  
+    void rotateArr(vector<int>& arr, int d) {
+        int n = arr.size();
+        d %= n;
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+};
