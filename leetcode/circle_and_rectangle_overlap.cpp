3332952a23f84ae39bdabf35252477f7d29ed409
Samyra312007 <samayra312007@gmail.com>
2026-09-19 15:50:37 +0530

    ADD: POTD Circle and Rectangle Overlap

---
+++ leetcode/circle_and_rectangle_overlap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int xi, yi;
+        if(x1 > xCenter) xi = x1;
+        else if(x2 < xCenter) xi = x2;
+        else xi = xCenter;
+        if(y1 > yCenter) yi = y1;
+        else if(y2 < yCenter) yi = y2;
+        else yi = yCenter;
+        int res = sqrt( pow((xi-xCenter), 2) + pow((yi-yCenter), 2));
+        if(res <= radius){
+            return true;
+        }
+        return false;
+    }
+};
